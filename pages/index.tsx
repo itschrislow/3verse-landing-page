aa2c452bc572307d8735295fdec0f786421b1e73
chris0610 <chrislow360@gmail.com>
2022-05-07 17:53:02 +0800

    create landing page

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,84 +1,53 @@
-import type { NextPage } from 'next'
-import Head from 'next/head'
-import Image from 'next/image'
+import type { NextPage } from "next"
 
 const Home: NextPage = () => {
   return (
-    <div className="flex min-h-screen flex-col items-center justify-center py-2">
-      <Head>
-        <title>Create Next App</title>
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
-
-      <main className="flex w-full flex-1 flex-col items-center justify-center px-20 text-center">
-        <h1 className="text-6xl font-bold">
-          Welcome to{' '}
-          <a className="text-blue-600" href="https://nextjs.org">
-            Next.js!
-          </a>
+    <div className="flex h-screen w-full items-center justify-center bg-transparent p-10 sm:p-5">
+      <div className="opacity- absolute -z-10 h-screen w-full bg-black opacity-100">
+        <video autoPlay loop className="h-full w-full object-cover opacity-50">
+          {/* NOTE: update video source here */}
+          <source src="/static/video.mp4" type="video/mp4" />
+        </video>
+      </div>
+      <div className="mx-auto max-w-4xl text-center">
+        <h1 className="text-3xl font-extrabold text-white sm:text-4xl">
+          Trusted by developers from over 80 planets
         </h1>
-
-        <p className="mt-3 text-2xl">
-          Get started by editing{' '}
-          <code className="rounded-md bg-gray-100 p-3 font-mono text-lg">
-            pages/index.tsx
-          </code>
-        </p>
-
-        <div className="mt-6 flex max-w-4xl flex-wrap items-center justify-around sm:w-full">
-          <a
-            href="https://nextjs.org/docs"
-            className="mt-6 w-96 rounded-xl border p-6 text-left hover:text-blue-600 focus:text-blue-600"
-          >
-            <h3 className="text-2xl font-bold">Documentation &rarr;</h3>
-            <p className="mt-4 text-xl">
-              Find in-depth information about Next.js features and its API.
-            </p>
-          </a>
-
-          <a
-            href="https://nextjs.org/learn"
-            className="mt-6 w-96 rounded-xl border p-6 text-left hover:text-blue-600 focus:text-blue-600"
-          >
-            <h3 className="text-2xl font-bold">Learn &rarr;</h3>
-            <p className="mt-4 text-xl">
-              Learn about Next.js in an interactive course with quizzes!
-            </p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/canary/examples"
-            className="mt-6 w-96 rounded-xl border p-6 text-left hover:text-blue-600 focus:text-blue-600"
-          >
-            <h3 className="text-2xl font-bold">Examples &rarr;</h3>
-            <p className="mt-4 text-xl">
-              Discover and deploy boilerplate example Next.js projects.
-            </p>
-          </a>
-
-          <a
-            href="https://vercel.com/import?filter=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className="mt-6 w-96 rounded-xl border p-6 text-left hover:text-blue-600 focus:text-blue-600"
-          >
-            <h3 className="text-2xl font-bold">Deploy &rarr;</h3>
-            <p className="mt-4 text-xl">
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
+        <h2 className="mt-3 text-xl text-gray-300 sm:mt-4">
+          Lorem ipsum dolor, sit amet consectetur adipisicing elit. Repellendus
+          repellat laudantium.
+        </h2>
+        <div className="mt-10 pb-12 sm:pb-16">
+          <div className="mx-auto max-w-4xl">
+            <dl className="rounded-lg bg-white shadow-lg sm:grid sm:grid-cols-3">
+              <div className="flex flex-col border-b border-gray-100 p-6 text-center sm:border-0 sm:border-r">
+                <dt className="order-2 mt-2 text-lg font-medium leading-6 text-gray-500">
+                  Pepperoni
+                </dt>
+                <dd className="order-1 text-5xl font-extrabold text-emerald-500">
+                  100%
+                </dd>
+              </div>
+              <div className="flex flex-col border-t border-b border-gray-100 p-6 text-center sm:border-0 sm:border-l sm:border-r">
+                <dt className="order-2 mt-2 text-lg font-medium leading-6 text-gray-500">
+                  Delivery
+                </dt>
+                <dd className="order-1 text-5xl font-extrabold text-emerald-500">
+                  24/7
+                </dd>
+              </div>
+              <div className="flex flex-col border-t border-gray-100 p-6 text-center sm:border-0 sm:border-l">
+                <dt className="order-2 mt-2 text-lg font-medium leading-6 text-gray-500">
+                  Calories
+                </dt>
+                <dd className="order-1 text-5xl font-extrabold text-emerald-500">
+                  100k
+                </dd>
+              </div>
+            </dl>
+          </div>
         </div>
-      </main>
-
-      <footer className="flex h-24 w-full items-center justify-center border-t">
-        <a
-          className="flex items-center justify-center gap-2"
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <Image src="/vercel.svg" alt="Vercel Logo" width={72} height={16} />
-        </a>
-      </footer>
+      </div>
     </div>
   )
 }
